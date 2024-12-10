f793cc699a65c7fa9ed9ea2024d7021c53d2d4ae
cipisek8 <cipisek@pecenka.name>
2024-12-10 13:02:27 +0100

    added date

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -5,16 +5,18 @@ import { DATABASE_URL, FetchDatabase } from '../App'
 function CreateTask({ setTasks }: { setTasks: any }) {
 
     const [description, setDescription] = useState<string>("")
+    const [date, setDate] = useState<Date>(new Date())
 
     return (
         <div id="create_task">
             <input type="text" id="description" onInput={(e) => setDescription((e.target as HTMLInputElement).value)} />
-            <button onClick={() => HandleCreate(description, setTasks)}>Create</button>
+            <input type='date' id='date' min={new Date(Date.now()).toISOString().split('T')[0]} onInput={(e) => setDate(new Date((e.target as HTMLInputElement).value))}></input>
+            <button onClick={() => HandleCreate(description, setTasks, date)}>Create</button>
         </div>
     )
 }
 
-function HandleCreate(description: string, setTasks: any) {
+function HandleCreate(description: string, setTasks: any, date: Date) {
     if (description == "")
         return;
 
@@ -23,7 +25,7 @@ function HandleCreate(description: string, setTasks: any) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ title: description, completed: false })
+        body: JSON.stringify({ title: description, completed: false, finishDate: date.getTime() })
     })
 
     FetchDatabase(setTasks);
